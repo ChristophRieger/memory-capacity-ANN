ada59d0b9686294a18cfa658e27f30fe786a50de
Christoph Rieger <christoph.rieger@outlook.com>
2023-11-25 14:55:14 +0100

    cleanup

--- main.py
+++ main.py
@@ -31,20 +31,15 @@ class OneLayerModel(torch.nn.Module):
 
   def __init__(self, inputSize, outputSize, my_device):
       super(OneLayerModel, self).__init__()
-      # self.inputLayer = torch.nn.Linear(inputSize, inputSize)
       self.fullyConnectedLayer = torch.nn.Linear(inputSize, outputSize)
       self.activation = torch.nn.Sigmoid()
-      # self.softmax = torch.nn.Softmax()
       # !!! This has to be called last, otherwise it doesn't move the parameters
       # above to the device
       self.to(my_device) # move the model to the same device as tensors
 
   def forward(self, x):
-      # x = self.inputLayer(x)
-      # x = self.activation(x)
       x = self.fullyConnectedLayer(x)
       x = self.activation(x)
-      # x = self.softmax(x)
       return x
 
 def train_one_epoch(epoch_index, training_loader, model, optimizer, loss_fn, my_batch_size, my_device, tb_writer):
@@ -57,25 +52,13 @@ def train_one_epoch(epoch_index, training_loader, model, optimizer, loss_fn, my_
     for i, data in enumerate(training_loader):
         # Every data instance is an input + label pair
         inputs, labels = data
-        # fix (this was only needed locally, on cluster it worked) acc. to https://stackoverflow.com/questions/69742930/runtimeerror-nll-loss-forward-reduce-cuda-kernel-2d-index-not-implemented-for
-        # now not needed anymore XD
-        # labels = labels.type(torch.LongTensor)
-        # labels = labels.to(my_device)
         # Zero your gradients for every batch!
         optimizer.zero_grad()
 
         # Make predictions for this batch
-        # print(model)
         outputs = model(inputs)
 
         # Compute the loss and its gradients
-        # output has size of (nOutput, batchSize)
-        # print("inputs")
-        # print(inputs)
-        # print("outputs")
-        # print(outputs)
-        # print("labels")
-        # print(labels)
         loss = loss_fn(outputs, labels)
         loss.backward()
 
@@ -84,13 +67,6 @@ def train_one_epoch(epoch_index, training_loader, model, optimizer, loss_fn, my_
 
         # Gather data and report
         running_loss += loss.item()
-        # I commented code below out, as I do not need the loss within one epoch..
-        # if i % my_batch_size == my_batch_size - 1:
-        #     last_loss = running_loss / my_batch_size # loss per batch
-        #     # print('  batch {} loss: {}'.format(i + 1, last_loss))
-        #     tb_x = epoch_index * len(training_loader) + i + 1
-        #     tb_writer.add_scalar('Loss/train', last_loss, tb_x)
-        #     running_loss = 0.
     last_loss = running_loss
     return last_loss / len(training_loader)
 
@@ -138,7 +114,7 @@ N = 20
 sparsity = 0.1 # fraction of active bits in data
 dataset_size = 40
 my_batch_size = 1
-EPOCHS = 3000
+EPOCHS = 5
 learning_rate = 0.01
 momentum = 0.9
 
@@ -153,111 +129,52 @@ else:
     my_device = torch.device('cpu')
 print('Device: {}'.format(my_device))
 
-
-
 X, y = create_dataset(N, sparsity, dataset_size) 
 
-# sys.exit()
-# print("X")
-# print(X)
-# print("y")
-# print(y)
-# I set train and validation dataset equal, as I need the same random data to
-# check if a pattern was memorized
 dataset_train = customTensorDataset(X, y, my_device)
-dataset_validation = customTensorDataset(X, y, my_device)
 train_loader = DataLoader(dataset=dataset_train, batch_size=my_batch_size, shuffle=True)
-validation_loader = DataLoader(dataset=dataset_validation, batch_size=1, shuffle=False)
-# for index, data in enumerate(train_loader):
-  # access input vector (dont know why we need double 0... its in double square brackets..)
-  # print(data[0][0])  
-  # access value of input vector
-  # print(data[0][0][2])
-  # access label
-  # print(data[1][0])
 oneLayerModel = OneLayerModel(N, N, my_device)
 if load_model:
   oneLayerModel.load_state_dict(torch.load('path_to_model'))
-# print('The model:')
-# print(oneLayerModel)
-# print('The parameters initially:')
-# for param in oneLayerModel.parameters():
-#   print(param)
 
 # Optimizers specified in the torch.optim package
 optimizer = torch.optim.SGD(oneLayerModel.parameters(), lr=learning_rate, momentum=momentum)
-
-# it seems this is the wrong loss function, as it is not supposed to be used
-# for multiple binary classes
-# loss_fn = torch.nn.CrossEntropyLoss()
 
 # I try Binary Cross Entropy Loss...
 loss_fn = torch.nn.BCELoss()
 # !!! TODO There is BCEWITHLOGITSLOSS which combines this loss with also applying 
 # the sigmoid function, and it says its prefered to use the 2 separately...
 
-# Loss Debugging
-# outputs = np.zeros((1, 10), dtype='float32')
-# outputs[0,0] = 1
-# print("my outputs")
-# print(outputs)
-# labels = np.zeros((1, 10), dtype='float32')
-# labels[0,0] = 0.8
-# print("my labels")
-# print(labels)
-# outputss = torch.tensor(outputs, device=my_device)
-# labelss = torch.tensor(labels, device=my_device)
-
-# print("myloss")
-# print(loss_fn(outputss, labelss))
-
 # Initializing in a separate cell so we can easily add more epochs to the same run
 writer = SummaryWriter(results_path)
 epoch_number = 0
 
-best_vloss = 1_000_000.
+best_loss = 1_000_000.
 if not os.path.exists('modelStates'):
   os.mkdir('modelStates')
 if not os.path.exists(model_state_path):
   os.mkdir(model_state_path)
 for epoch in range(EPOCHS):
-    # print('EPOCH {}:'.format(epoch_number + 1))
+    print('EPOCH {}:'.format(epoch_number + 1))
 
     # Make sure gradient tracking is on, and do a pass over the data
     oneLayerModel.train(True)
     avg_loss = train_one_epoch(epoch_number, train_loader, oneLayerModel, optimizer, loss_fn, my_batch_size, my_device, writer)
 
-    running_vloss = 0.0
     # Set the model to evaluation mode, disabling dropout and using population
     # statistics for batch normalization.
     oneLayerModel.eval()
-
-    # Disable gradient computation and reduce memory consumption.
-    with torch.no_grad():
-        for i, vdata in enumerate(validation_loader):
-            vinputs, vlabels = vdata
-            # fix (this was only needed locally, on cluster it worked) acc. to https://stackoverflow.com/questions/69742930/runtimeerror-nll-loss-forward-reduce-cuda-kernel-2d-index-not-implemented-for
-            # now not needed anymore XD
-            # vlabels = vlabels.type(torch.LongTensor)
-            # vlabels = vlabels.to(my_device)
             
-            voutputs = oneLayerModel(vinputs)
-            vloss = loss_fn(voutputs, vlabels)
-            running_vloss += vloss
-            
-    avg_vloss = running_vloss / (i + 1)
-    print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
+    print('LOSS train {}'.format(avg_loss))
 
-    # Log the running loss averaged per batch
-    # for both training and validation
-    writer.add_scalars('Training vs. Validation Loss',
-                    { 'Training' : avg_loss, 'Validation' : avg_vloss },
+    writer.add_scalars('Training',
+                    { 'Training' : avg_loss },
                     epoch_number + 1)
     writer.flush()
 
     # Track best performance, and save the model's state
-    if avg_vloss < best_vloss:
-        best_vloss = avg_vloss
+    if avg_loss < best_loss:
+        best_loss = avg_loss
         torch.save(oneLayerModel.state_dict(), model_state_path + '/model_{}'.format(epoch_number))
 
     epoch_number += 1
@@ -270,17 +187,15 @@ np.savetxt(results_path + "/y.txt", y, fmt='%d')
 
 # calculate percentage of correctly learned patterns
 correct_y_predictions = np.zeros(((1,1)))
-for i, vdata in enumerate(validation_loader):
-  vinputs, vlabels = vdata
-  y_prediction = oneLayerModel(vinputs)
+for i, data in enumerate(train_loader):
+  inputs, labels = data
+  y_prediction = oneLayerModel(inputs)
   for j in range(y_prediction.size()[1]):
     if y_prediction[0][j] >= 0.5:
       y_prediction[0][j] = 1
     else:
       y_prediction[0][j] = 0
-  # this returns a vector, but I want a single bool
-  # if y_prediction[0] == vlabels[0]:
-  if torch.equal(y_prediction, vlabels):
+  if torch.equal(y_prediction, labels):
     correct_y_predictions[0] += 1
     
 percentage_of_correct_memorizations = correct_y_predictions[0] / len(y)
