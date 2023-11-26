535f3fad4cbfd1e38c735081ee7a4d4b8dd53044
Christoph Rieger <christoph.rieger@outlook.com>
2023-11-26 17:21:51 +0100

    added memoryPlot

---
+++ memoryPlot.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 26 17:12:11 2023
+
+@author: chris
+"""
+
+import matplotlib.pyplot as plt
+
+# TODO: increase resolution for small N
+
+sparsity = 0.1
+
+N = [10, 20, 30, 40, 50, 75, 100]
+# This number is taken from the biggest dataset_size where 100% of patterns are 
+# memorized
+number_of_patterns = [10, 30, 50, 70, 90, 160, 260]
+
+plt.close("all")
+plt.figure()  
+plt.plot(N, number_of_patterns)
+plt.title("Number of correctly memorzized patterns", fontsize=14)
+plt.ylabel("patterns", fontsize=12)
+plt.xlabel("N", fontsize=12)
+plt.tick_params(axis='both', labelsize=11)
+plt.savefig("memoryPlot" + ".svg")  
+plt.savefig("memoryPlot" + ".png")
