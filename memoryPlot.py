58a0d5d31d2f4b2f7ea2d8aea945c963c3c49ea8
Christoph Rieger <christoph.rieger@outlook.com>
2023-12-18 16:54:37 +0100

    added new results

--- memoryPlot.py
+++ memoryPlot.py
@@ -6,27 +6,56 @@ Created on Sun Nov 26 17:12:11 2023
 """
 
 import matplotlib.pyplot as plt
+import sys
 
-# TODO: increase resolution for small N
 
+#  simple loss
+# at N200 i increased best_loss - avg_loss from < 10 ** -4 to < 10**-5
+# N =                  [10, 20, 30, 40, 50, 75 , 100, 200, 250, 400, 600, 800, 1000, 2000 ]
+# number_of_patterns = [10, 30, 50, 70, 90, 160, 260, 520, 610, 975, 1280, 1720, 2030, 2500 ]
+
+############### simple loss ############
 sparsity = 0.1
 
-# at N200 i increased best_loss - avg_loss from < 10 ** -4 to < 10**-5
-N =                  [10, 20, 30, 40, 50, 75 , 100, 200, 250, 400, 600, 800, 1000, 2000 ]
-# This number is taken from the biggest dataset_size where 100% of patterns are 
-# memorized
-# !!! 2500 is only 99.5% accurate, but 2400 also had 99.5%
-number_of_patterns = [10, 30, 50, 70, 90, 160, 260, 520, 610, 975, 1280, 1720, 2030, 2500 ]
+N = []
+number_of_patterns = []
+mean_of_patterns = []
+std = []
+dataset_size = []
+
+N.append(100)
+number_of_patterns.append(283)
+mean_of_patterns.append(243)
+std.append(23.95)
+dataset_size.append(300)
+
+N.append(200)
+number_of_patterns.append(580)
+mean_of_patterns.append(550)
+std.append(32)
+dataset_size.append(600)
+
+# N.append(300)
+# number_of_patterns.append()
+# mean_of_patterns.append()
+# std.append()
+# dataset_size.append()
+
+# N.append(400)
+# number_of_patterns.append()
+# mean_of_patterns.append()
+# std.append()
+# dataset_size.append()
 
 plt.close("all")
 plt.figure()  
 plt.plot(N, number_of_patterns, marker = 'o', ms = 4)
+plt.errorbar(N, mean_of_patterns, std, linestyle='None', marker = 'x', ms = 4)
 plt.title("", fontsize=14)
 plt.ylabel("# of memorzized patterns", fontsize=12)
 plt.xlabel("N", fontsize=12)
 plt.tick_params(axis='both', labelsize=11)
 plt.savefig("memoryPlot" + ".png")
-
 plt.figure()  
 plt.plot(N, [i / j for i, j in zip(number_of_patterns, N)], marker = 'o', ms = 4)
 plt.title("", fontsize=14)
@@ -34,7 +63,6 @@ plt.ylabel("# of memorized patterns / N", fontsize=12)
 plt.xlabel("N", fontsize=12)
 plt.tick_params(axis='both', labelsize=11)
 plt.savefig("slope" + ".png")
-
 plt.figure()  
 plt.plot(N, [i / j**2 for i, j in zip(number_of_patterns, N)], marker = 'o', ms = 4)
 plt.title("", fontsize=14)
@@ -43,38 +71,59 @@ plt.xlabel("N", fontsize=12)
 plt.tick_params(axis='both', labelsize=11)
 plt.savefig("mByN2" + ".png")
 
+# sys.exit()
 
+##### custom loss ###############
+tolerance = 0.1
+sparsity = 0.1
+N = []
+number_of_patterns = []
+mean_of_patterns = []
+std = []
+dataset_size = []
 
-#  custom loss!!!!!!!!
-# at N200 i increased best_loss - avg_loss from < 10 ** -4 to < 10**-5
-N =                  [10, 20, 30, 40, 50, 75 , 100, 200, 250, 400, 600, 800, 1000, 2000 ]
-# This number is taken from the biggest dataset_size where 100% of patterns are 
-# memorized
-# !!! 2500 is only 99.5% accurate, but 2400 also had 99.5%
-number_of_patterns = [??, 29 (mit 40) ]
+N.append(100)
+number_of_patterns.append(337)
+mean_of_patterns.append(305)
+std.append(24)
+dataset_size.append(350)
 
-plt.close("all")
+N.append(200)
+number_of_patterns.append(653)
+mean_of_patterns.append(0)
+std.append(5)
+
+# N.append(300)
+# number_of_patterns.append()
+# mean_of_patterns.append()
+# std.append()
+# dataset_size.append()
+
+# N.append(400)
+# number_of_patterns.append()
+# mean_of_patterns.append()
+# std.append()
+# dataset_size.append()
+
 plt.figure()  
 plt.plot(N, number_of_patterns, marker = 'o', ms = 4)
+plt.errorbar(N, mean_of_patterns, std, linestyle='None', marker = 'x', ms = 4)
 plt.title("", fontsize=14)
 plt.ylabel("# of memorzized patterns", fontsize=12)
 plt.xlabel("N", fontsize=12)
 plt.tick_params(axis='both', labelsize=11)
-plt.savefig("memoryPlot" + ".png")
-
+plt.savefig("memoryPlotCustom" + ".png")
 plt.figure()  
 plt.plot(N, [i / j for i, j in zip(number_of_patterns, N)], marker = 'o', ms = 4)
 plt.title("", fontsize=14)
 plt.ylabel("# of memorized patterns / N", fontsize=12)
 plt.xlabel("N", fontsize=12)
 plt.tick_params(axis='both', labelsize=11)
-plt.savefig("slope" + ".png")
-
+plt.savefig("slopeCustom" + ".png")
 plt.figure()  
 plt.plot(N, [i / j**2 for i, j in zip(number_of_patterns, N)], marker = 'o', ms = 4)
 plt.title("", fontsize=14)
 plt.ylabel("# of memorized patterns / $N^2$", fontsize=12)
 plt.xlabel("N", fontsize=12)
 plt.tick_params(axis='both', labelsize=11)
-plt.savefig("mByN2" + ".png")
-
+plt.savefig("mByN2Custom" + ".png")
